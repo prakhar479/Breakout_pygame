1ae854f3687df19b98375ac08adcd9f7274edec1
Prakhar-Jain-1 <prakharjain582@gmail.com>
2024-04-10 23:54:21 +0530

    paddle side collision left

---
+++ game/blocks.py
@@ -0,0 +1,72 @@
+import pygame
+import random
+import math
+from constants import *  
+
+class BlockArray:
+    def __init__(self, screen, num_blocks, lines, block_width=55, block_height=35, block_spacing=5):
+        self.screen = screen
+        self.num_blocks = num_blocks
+        self.block_width = block_width
+        self.block_height = block_height
+        self.block_spacing = block_spacing
+        self.lines = lines
+        self.n = self.num_blocks * self.lines
+
+    def initialize_block_pos(self):
+        x = (self.screen.get_width() - self.num_blocks * (self.block_width + self.block_spacing)) // 2
+        y = 50  # Top margin for the blocks
+        self.block_pos = []
+        for _ in range(self.lines):
+            y += 40
+            for i in range(self.num_blocks):
+                # Calculate position of each block
+                block_x = x + i * (self.block_width + self.block_spacing)
+                block_y = y
+                color = random.choice([RED, GREEN, BLUE, YELLOW, CYAN, MAGENTA, ORANGE, PINK, PURPLE,])
+                self.block_pos.append((block_x,block_y,color))
+
+
+    def draw_blocks(self):
+
+        for i in range(self.n):
+            # Calculate position of each block
+            block_x = self.block_pos[i][0]
+            block_y = self.block_pos[i][1]
+            color = self.block_pos[i][2]
+
+            # Draw block
+            pygame.draw.rect(self.screen, color, (block_x, block_y, self.block_width, self.block_height))
+
+    def update(self, ball):
+        # n = len(self.block_pos)
+        for i in range(self.n):
+
+            block_x, block_y, _ = self.block_pos[i]
+            if self.is_ball_colliding_with_block(ball, block_x, block_y):
+                
+                ball_center = (ball.position[0], ball.position[1])
+
+                closest_x = max(block_x, min(ball_center[0], block_x + self.block_width))
+                closest_y = max(block_y, min(ball_center[1], block_y + self.block_height))
+                self.n-=1
+                del self.block_pos[i]
+
+                # Determine the side of the block where the collision occurred
+                if closest_x == block_x or closest_x == block_x + self.block_width:
+                    return 0,-1
+                elif closest_y == block_y or closest_y == block_y + self.block_height:
+                    return 1, -1
+                
+        return 0,1
+
+    def is_ball_colliding_with_block(self, ball, block_x, block_y):
+        # Calculate the center of the ball
+        ball_center = (ball.position[0], ball.position[1])
+        # Calculate closest point on block to the ball's center
+        closest_x = max(block_x, min(ball_center[0], block_x + self.block_width))
+        closest_y = max(block_y, min(ball_center[1], block_y + self.block_height))
+        # Calculate distance between ball's center and closest point on block
+        distance = math.sqrt((ball_center[0] - closest_x) ** 2 + (ball_center[1] - closest_y) ** 2)
+        return distance <= ball.radius
+
---
+++ game/main.py
@@ -0,0 +1,64 @@
+import pygame
+from paddle import Paddle
+from constants import *
+from ball import Ball
+from blocks import BlockArray
+# Initialize Pygame
+pygame.init()
+
+# Screen dimensions
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Break Out")
+
+
+paddle = Paddle(screen, PADDLE_WIDTH, PADDLE_HEIGHT, WHITE)
+
+ball = Ball(screen, BALL_RADIUS, BLUE, (SCREEN_WIDTH//2,SCREEN_HEIGHT - PADDLE_HEIGHT - BALL_RADIUS))
+blocks = BlockArray(screen, BLOCK_COLUMNS, BLOCK_ROWS)
+blocks.initialize_block_pos()
+
+
+
+# Game loop
+running = True
+clock = pygame.time.Clock()
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                ball.velocity[0]+=BALL_VELOCITY
+                ball.velocity[1]-=BALL_VELOCITY
+            if event.key == pygame.K_LEFT:
+                paddle.speed = -PADDLE_SPEED  # Move left
+            elif event.key == pygame.K_RIGHT:
+                paddle.speed = PADDLE_SPEED  # Move right
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                 paddle.speed = 0 
+
+    paddle.update()
+    PADDLE_SPEED*=FRAME_MULT
+
+    ball.update(paddle,blocks)
+
+    if ball.position[1] > SCREEN_HEIGHT:
+        running = False
+
+    screen.fill(BLACK)
+
+    # Draw the sprite
+    paddle.draw()
+    ball.draw()
+    blocks.draw_blocks()
+    # Update the display
+    pygame.display.flip()
+
+    # Cap the frame rate
+    clock.tick(60)
+
+# Quit Pygame
+pygame.quit()
